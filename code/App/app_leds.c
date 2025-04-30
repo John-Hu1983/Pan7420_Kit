cf86a4145ea0542b55656f8b7f434215f9162685
johnhu <johnh@spinmaster.com>
2025-04-30 10:14:16 +0800

    add LED display event

--- code/App/app_leds.c
+++ code/App/app_leds.c
@@ -7,13 +7,107 @@ static event_state schedule = {
 led_ctr lamps[2];
 
 /*
- * @description	:   about 0.1us
+ * @description	:
+ * @param		:
+ * @return		:
+ */
+void set_led_patt(u8 cn, LED_Patt_t pat, u16 argu)
+{
+    led_ctr *led;
+
+    led = (cn == 0) ? &lamps[0] : &lamps[1];
+    if (led->patt == pat && led->argument == argu)
+    {
+        return;
+    }
+    memset((void *)led, 0, sizeof(led));
+    led->patt = pat;
+    led->argument = argu;
+}
+
+/*
+ * @description	:
+ * @param		:
+ * @return		:
+ */
+void perform_led_display(u8 cn, osvar_t ms)
+{
+    led_ctr *led;
+
+    led = (cn == 0) ? &lamps[0] : &lamps[1];
+    led->ms += ms;
+    switch (led->step)
+    {
+    case 0:
+        if (led->patt == led_patt_on)
+        {
+            led->step = 10;
+        }
+        else if (led->patt == led_patt_flicker)
+        {
+            led->step = 20;
+        }
+        else
+        {
+            led->step = 0xff;
+        }
+        break;
+
+    case 10:
+        control_leds_condition(cn, LED_Lev_on);
+        break;
+
+    case 20:
+        if (led->argument == 0)
+        {
+            led->step = 0xff;
+        }
+        else if (led->argument == 0xffff)
+        {
+            led->ms = 0;
+            led->step++;
+        }
+        else
+        {
+            led->ms = 0;
+            led->argument--;
+            led->step++;
+        }
+        break;
+
+    case 21:
+        control_leds_condition(cn, LED_Lev_on);
+        if (led->ms >= 200)
+        {
+            led->ms = 0;
+            led->step++;
+        }
+        break;
+
+    case 22:
+        control_leds_condition(cn, LED_Lev_off);
+        if (led->ms >= 200)
+        {
+            led->ms = 0;
+            led->step = 20;
+        }
+        break;
+
+    default:
+        control_leds_condition(cn, LED_Lev_off);
+        break;
+    }
+}
+
+/*
+ * @description	:
  * @param		:
  * @return		:
  */
 enum
 {
     LED_step_init = 0,
+    LED_step_loop,
 };
 void task_leds_display(osvar_t ms)
 {
@@ -23,6 +117,16 @@ void task_leds_display(osvar_t ms)
     case LED_step_init:
         memset((void *)lamps, 0, sizeof(lamps));
         init_leds_object();
+
+        set_led_patt(0, led_patt_on, 0xffff);
+        set_led_patt(1, led_patt_flicker, 0xffff);
+
+        schedule.bits.step = LED_step_loop;
+        break;
+
+    case LED_step_loop:
+        perform_led_display(0, ms);
+        perform_led_display(1, ms);
         break;
     }
 }
--- code/App/app_leds.h
+++ code/App/app_leds.h
@@ -3,6 +3,12 @@
 
 typedef enum
 {
+  LED_Lev_on = 0,
+  LED_Lev_off,
+} LED_LEV_t;
+
+typedef enum
+{
   led_patt_off = 0,
   led_patt_on,
   led_patt_flicker,
@@ -12,7 +18,10 @@ typedef struct
 {
   LED_Patt_t patt;
   u16 argument;
+  u16 ms;
+  u8 step;
 } led_ctr;
 
 void task_leds_display(osvar_t ms);
+void set_led_patt(u8 cn, LED_Patt_t pat, u16 argu);
 #endif
